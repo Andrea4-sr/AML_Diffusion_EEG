9dcf75a1409dc5e7a753f697217a581afd9fa86d
avocardio <mkalcher@student.ethz.ch>
2024-05-11 01:19:29 +0200

    Sample from pretrained model file

--- src/sample_from_model.py
+++ src/sample_from_model.py
@@ -1,44 +1,72 @@
-import torch
+import os
+import numpy as np
 import matplotlib.pyplot as plt
+import torch
 
-from model import Unet1D, GaussianDiffusion1D
-
-
-def standarize(t):
-    return (t - t.mean()) / t.std()
+from models.custom_cond_model import Unet1D, GaussianDiffusion1D
 
 
-if __name__ == "__main__":
-
-    import matplotlib.pyplot as plt
+def sample_from_model(model_path, num_samples=1, label=None, output_directory=None):
 
     backbone = Unet1D(
         dim=64,
         dim_mults=(1, 2, 4, 8),
         channels=1,
         condition=True,
-        condition_type='time_mlp',  # 'input_concat', 'input_add', 'attn', 'attn_mlp', 'attn_embed', 'time_mlp'
+        condition_classes=2,
+        condition_type='attn_mlp',
     )
 
     model = GaussianDiffusion1D(
         backbone,
         seq_length=1000,
         timesteps=1000,
-        objective='pred_v'
+        objective='pred_x0'
     )
 
-    model.load_state_dict(torch.load("tests/results/model-7.pt")['model'])
-
+    model.load_state_dict(torch.load(model_path)['model'], strict=True)
     model.eval().cuda()
 
-    total_params = sum(p.numel() for p in model.parameters())
-    print(f"Total number of parameters: {total_params}")
+    # total_params = sum(p.numel() for p in model.parameters())
+    # print(f"Total number of parameters: {total_params}")
 
     with torch.no_grad():
-        samples = model.sample(batch_size=4)
+        if label == 0:
+            samples = model.sample(batch_size=num_samples, label=torch.zeros([num_samples], dtype=torch.float32).cuda().unsqueeze(1))
+        elif label == 1:
+            samples = model.sample(batch_size=num_samples, label=torch.ones([num_samples], dtype=torch.float32).cuda().unsqueeze(1))
+        else:
+            samples = model.sample(batch_size=num_samples)
 
-    plt.figure(figsize=(10, 5))
-    for n,i in enumerate(samples.cpu()): 
-        plt.plot(i.squeeze())
-        # plt.show()
-    plt.savefig(f"test.png")
+    if output_directory is not None:
+        os.makedirs(output_directory, exist_ok=True)
+
+        for i, sample in enumerate(samples):
+            file_path = os.path.join(output_directory, f'sample_{i+1}.npy')
+            np.save(file_path, sample.cpu().numpy())
+    
+    return samples
+
+
+if __name__ == "__main__":
+
+
+    # model = "results\++ long 2 class Unet1D attn mlp - pred_x0\model-50.pt"  # ************ Weird depressions
+    # model = "results\+ comb long 2 class Unet1D attn mlp - pred_x0\model-50.pt" # ************ Too noisy healthy data
+    model = "results\++ 2 Unet1D attn mlp - pred_x0\model-15.pt" # ************ This one is good !
+
+    seizure_samples = sample_from_model(model, num_samples=5, label=1, output_directory=None)
+    healthy_samples = sample_from_model(model, num_samples=5, label=0, output_directory=None)
+
+    plt.figure(figsize=(10, 20)) 
+    for i in range(4):
+        plt.subplot(4, 2, 2 * i + 1)
+        plt.plot(healthy_samples[i].squeeze().cpu().numpy()[50:-50])
+        plt.title('Healthy Sample ' + str(i + 1))
+
+        plt.subplot(4, 2, 2 * i + 2)
+        plt.plot(seizure_samples[i].squeeze().cpu().numpy()[50:-50])
+        plt.title('Seizure Sample ' + str(i + 1))
+
+    plt.tight_layout()
+    plt.show()
