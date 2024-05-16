11ce53d70ef90fee79c6bbd6cf4b6b476bb4c8a1
avocardio <mkalcher@student.ethz.ch>
2024-05-17 00:26:07 +0200

    Updated shell scripts and readme with citations

--- train.sh
+++ train.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# python src/prepare_eeg_dataset.py --input_path data/seizure_data/train --output_path data/train_250hz_05_70_n60_CZ --duration 4 --channels "CZ" --classes "fnsz,gnsz" --frequency 250 --notch_filter 60 --lowcut 0.5 --highcut 70
-
-
-# python src/main.py --batch_size 64 --train_steps 7000 --data_path "train_250hz_05_70_n60_CZ" --objective "pred_v" --wandb --name "Unet1D no condition - pred_v - 250hz_05_70_n60_CZ"
-
-# python src/main.py --batch_size 64 --train_steps 7000 --data_path "train_250hz_05_70_n60_CZ" --objective "pred_x0" --wandb --name "Unet1D no condition - pred_x0 - 250hz_05_70_n60_CZ"
-
-# python src/main.py --batch_size 64 --train_steps 7000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_v"  --wandb --name "Unet1D no condition - pred_v - 250hz_05_40_n00_CZ"
-
-# python src/main.py --batch_size 64 --train_steps 7000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_x0" --wandb --name "Unet1D no condition - pred_x0 - 250hz_05_40_n00_CZ"
+# python src/prepare_eeg_dataset.py --input_path data/seizure_data/train --output_path data/train_250hz_05_40_n00_CZ --duration 4 --channels "CZ" --classes "fnsz,gnsz" --frequency 250--lowcut 0.5 --highcut 40
 
 
 # python src/main.py --model custom_Unet1D --diffuser custom_GaussianDiffusion1D --batch_size 64 --train_steps 15000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_x0" --wandb --condition --condition_type "input_concat" --name "Unet1D input concat - pred_x0"
@@ -35,8 +26,3 @@
 python src/main.py --model custom_Unet1D --diffuser custom_GaussianDiffusion1D --batch_size 128 --train_steps 50000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_x0" --condition_classes 2 --wandb --condition --condition_type "time_mlp" --name "long 2 class Unet1D time mlp - pred_x0"
 
 python src/main.py --model custom_Unet1D --diffuser custom_GaussianDiffusion1D --batch_size 128 --train_steps 50000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_x0" --condition_classes 2 --wandb --condition --condition_type "attn_mlp" --name "long 2 class Unet1D attn mlp - pred_x0"
-
-
-python src/main.py --model custom_Unet1D --diffuser custom_GaussianDiffusion1D --dataset Combined_Dataset1D --class_labels "{'fnsz': 1.0}" --batch_size 64 --train_steps 15000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_x0" --condition_classes 2 --wandb --condition --condition_type "attn_mlp" --name "comb long 2 class Unet1D attn mlp - pred_x0"
-
-python src/main.py --model custom_Unet1D --diffuser custom_GaussianDiffusion1D --dataset Combined_Dataset1D --class_labels "{'fnsz': 1.0}" --batch_size 128 --train_steps 50000 --data_path "train_250hz_05_40_n00_CZ" --objective "pred_x0" --condition_classes 2 --wandb --condition --condition_type "attn_mlp" --name "comb long 2 class Unet1D attn mlp - pred_x0"
---
+++ validate.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+TRAIN_DATASET_REAL="data/real/classifier_train"
+VALIDATION_REAL="data/real/validation"
+TRAIN_DATASET_SYNTH="data/synthetic/classifier_train"
+VALIDATION_SYNTH="data/synthetic/validation"
+
+REAL_MODEL_DUMP_DIR="results/classifer_real"
+SYNTH_MODEL_DUMP_DIR="results/classifer_synth"
+SYNTH_MODEL_20k_DUMP_DIR="results/classifer_synth_20k"
+RESULTS_REAL_DIR="results/"
+RESULTS_SYNTH_DIR="results/"
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+# Train the classifier on real data
+echo "Training the classifier on real data..."
+python3 src/train_classifier_steps.py --dataset_path "$TRAIN_DATASET_REAL" --model_dump_path "$REAL_MODEL_DUMP_DIR"
+
+# Evaluate the classifier on real data (eval)
+echo "Evaluating the classifier on real data..."
+python3 src/evaluate_classifier_plot.py --model_path "$REAL_MODEL_DUMP_DIR" --dataset_path "$VALIDATION_REAL" --plot_path "$RESULTS_REAL_DIR" --metrics_path "$RESULTS_REAL_DIR/" --metrics_file_name "SVM_train_real_test_real" --plot_name "SVM_train_real_test_real"
+
+# Evaluate the classifier on synthetic data
+echo "Evaluating the classifier on synthetic data..."
+python3 src/evaluate_classifier_plot.py --model_path "$REAL_MODEL_DUMP_DIR" --dataset_path "$VALIDATION_SYNTH" --plot_path "$RESULTS_REAL_DIR" --metrics_path "$RESULTS_REAL_DIR" --metrics_file_name "SVM_train_real_test_synth" --plot_name "SVM_train_real_test_synth"
+
+# Train the classifier on synthetic data
+echo "Training the classifier on synthetic data..."
+python3 src/train_classifier_steps.py --dataset_path "$TRAIN_DATASET_SYNTH" --model_dump_path "$SYNTH_MODEL_DUMP_DIR"
+
+# Evaluate the classifier on synthetic data
+echo "Evaluating the classifier on synthetic data..."
+python3 src/evaluate_classifier_plot.py --model_path "$SYNTH_MODEL_DUMP_DIR" --dataset_path "$VALIDATION_SYNTH" --plot_path "$RESULTS_SYNTH_DIR" --metrics_path "$RESULTS_SYNTH_DIR" --metrics_file_name "SVM_train_synth_test_synth" --plot_name "SVM_train_synth_test_synth"
+
+# Train the classifier on synthetic data with 20k steps
+echo "Training the classifier on synthetic data with 20k steps... (this might take 5-10 mins)"
+python3 src/train_classifier_steps.py --dataset_path "$TRAIN_DATASET_SYNTH" --model_dump_path "$SYNTH_MODEL_20k_DUMP_DIR" --train_steps 20000
+
+# Evaluate the classifier on real data
+echo "Evaluating the classifier on real data..."
+python3 src/evaluate_classifier_plot.py --model_path "$SYNTH_MODEL_20k_DUMP_DIR" --dataset_path "$VALIDATION_REAL" --plot_path "$RESULTS_SYNTH_DIR" --metrics_path "$RESULTS_SYNTH_DIR" --metrics_file_name "SVM_train_synth_test_real" --plot_name "SVM_train_synth_test_real"
+
+
+echo "Done."
